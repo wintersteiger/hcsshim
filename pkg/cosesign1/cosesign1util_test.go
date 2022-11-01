645afe738ed505d386eca1a590b37b1c4041c3b6
Christoph M. Wintersteiger <cwinter@microsoft.com>
2022-11-01 16:00:23 +0000

    Wire freshly generated .pem/.cose into go tests

--- pkg/cosesign1/cosesign1util_test.go
+++ pkg/cosesign1/cosesign1util_test.go
@@ -47,6 +47,10 @@ func comparePEMs(pk1pem string, pk2pem string) bool {
 	return bytes.Equal(pk1der, pk2der)
 }
 
+/*
+	Decode a COSE_Sign1 document and check that we get the expected payload, issuer, keys, certs etc.
+*/
+
 func Test_UnpackAndValidateCannedFragment(t *testing.T) {
 	var unpacked UnpackedCoseSign1
 	unpacked, err := UnpackAndValidateCOSE1CertChain(FragmentCose, nil, nil, false, false)
